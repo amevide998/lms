40a0b0a7bc446ebad0ea457bdd9ffd602312eb87
amevide998 <hdsvidi@gmail.com>
2023-11-06 13:51:39 +0700

    add error handling for null url value

--- database/mongodb.ts
+++ database/mongodb.ts
@@ -1,14 +1,16 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 
-const dbUrl = process.env.MONGODB_URI;
+const dbUrl: string | undefined = process.env.MONGODB_URI;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(dbUrl)
-            .then((data: any) => {
-                console.info(`successfully connected to mongodb at ${data.connection.host}`);
-            });
+        if (dbUrl != null) {
+            await mongoose.connect(dbUrl)
+                .then((data: any) => {
+                    console.info(`successfully connected to mongodb at ${data.connection.host}`);
+                });
+        }
     } catch (error: any) {
         console.error(`Error: ${error.message}`);
         setTimeout(connectDB, 5000);
