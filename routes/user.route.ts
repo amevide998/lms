d57f0c74c42b21ae4f7d648d20a0f93aa032ab11
amevide998 <hdsvidi@gmail.com>
2023-11-06 15:08:41 +0700

    add login and logout

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import {activateUser, registrationUser} from "../controllers/user.controller";
+import {activateUser, loginUser, logoutUser, registrationUser} from "../controllers/user.controller";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registrationUser);
 userRouter.post("/activate", activateUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/logout", logoutUser);
+
 
 
 export default userRouter;
