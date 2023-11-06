d0cc64f7bab003a556167de2a14d1d8aadd08765
amevide998 <hdsvidi@gmail.com>
2023-11-06 13:53:25 +0700

    change contraint field avatar to false

--- models/user.model.ts
+++ models/user.model.ts
@@ -37,11 +37,11 @@ const userSchema: Schema<IUser> = new Schema({
     avatar: {
         public_id: {
             type: String,
-            required: true,
+            required: false,
         },
         url: {
             type: String,
-            required: true,
+            required: false,
         }
     },
     role: {
@@ -60,6 +60,8 @@ const userSchema: Schema<IUser> = new Schema({
             }
         }
     ]
+}, {
+    timestamps: true
 })
 
 
