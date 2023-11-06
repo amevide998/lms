3905701e5e0646e364842cb2857389f04fe4f19e
amevide998 <hdsvidi@gmail.com>
2023-11-06 15:09:18 +0700

    fixing error handling

--- database/redis.ts
+++ database/redis.ts
@@ -1,6 +1,11 @@
 import {Redis} from 'ioredis';
 import 'dotenv/config';
 
-const redis = new Redis(process.env.REDIS_URL);
+let redis: Redis;
+
+if(!process.env.REDIS_URL){
+    throw new Error("REDIS_URL not found");
+}
+redis = new Redis(process.env.REDIS_URL);
 
 export default redis;
