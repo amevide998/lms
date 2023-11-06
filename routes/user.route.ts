c0723fecb4a63688e4c97bcdeb6a67943527360c
amevide998 <hdsvidi@gmail.com>
2023-11-06 13:53:35 +0700

    add activate

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import {registrationUser} from "../controllers/user.controller";
+import {activateUser, registrationUser} from "../controllers/user.controller";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registrationUser);
+userRouter.post("/activate", activateUser);
+
 
 export default userRouter;
